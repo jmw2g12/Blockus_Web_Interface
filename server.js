b228e573abbd74f6f21bd36e9b27285d6e3e586d
Jack Walker <jmw2g12@soton.ac.uk>
2016-11-02 02:32:17 +0000

    Testing multiple games

--- server.js
+++ server.js
@@ -8,76 +8,62 @@ app.use(express.static(__dirname + '/public'))
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
-//Blokus logic
-console.log("hello world, you're in the server.js file");
+console.log("Hello world, you're in the server.js file");
+
 http = require('http');
 fs = require('fs');
 
-var board = [];
-var turn = 1;
-initBoard();
-var reply = "post-received";
-var dataCount = 0;
 
-function initBoard(){
+//Blokus logic
+var board = [];
+var turn = [];
+var reply = "post-received";
+var passwordList = [];
+
+function initBoard(password){
+		board[password] = [];
         for (i = 0; i < 14; i++){
-                board.push(new Array(14));
+                board[password].push(new Array(14));
                 for (j = 0; j < 14; j++){
-                        board[i][j] = 0;
+                        board[password][i][j] = 0;
                 }
         }
-        //console.log("init-ed board = " + board[0]);
+        console.log(board[password]);
 }
-function boardToMsg(){
-        return JSON.stringify(board.concat(turn));
+function boardToMsg(password){
+        return JSON.stringify(board[password].concat(turn));
 }
-function handlePieceMsg(piece,code){
-		//console.log("msgJSON = " + msgJSON);
-        //var msg = JSON.parse(msgJSON);
-        //var msgGo = parseInt(msg[0]);
-        //console.log("msg.length = " + msg.length);
-        //console.log("turn = " + msgGo);
-
-		/*
-        var blocks = [];
-        for (i = 1; i < msg.length; i++){
-                var x = msg[i][0];
-                var y = msg[i][1];
-                blocks.push([x,y]);
-        }
-        console.log("blocks = " + blocks);
-        */
-        addPieceToBoard(piece,code);
-        //go = (msgGo === 1) ? 2 : 1;
-}
-function addPieceToBoard(piece,code){
-		console.log("addPieceToBoard, code = " + code);
+function addPieceToBoard(piece,code,password){
         for (i = 0; i < piece.length; i++){
-                board[(piece[i][1]-1)][(piece[i][0]-1)] = code;
+                board[password][(piece[i][1]-1)][(piece[i][0]-1)] = code;
         }
-        console.log("addPieceToBoard, board:");
-        printBoard();
-        console.log("end of board");
+        printBoard(password);
 }
-function printBoard(){
-        for (i = 0; i < board.length; i++){
-                console.log("  " + board[i]);
+function printBoard(password){
+        for (i = 0; i < board[password].length; i++){
+                console.log("  " + board[password][i]);
         }
 }
-function switchTurn(){
-		if (turn == 1){
-   		 	turn = 2;
-    		console.log("turn was 1, changing to " + turn);
-    	}else if (turn == 2){
-    		turn = 1;
-    		console.log("turn was 2, changing to " + turn);
+function switchTurn(password){
+		if (turn[password] == 1){
+   		 	turn[password] = 2;
+    		console.log("turn was 1, changing to " + turn[password]);
+    	}else if (turn[password] == 2){
+    		turn[password] = 1;
+    		console.log("turn was 2, changing to " + turn[password]);
     	}else{
     		console.log("TURN IS NOT 1 OR 2!!! ****");
     	}
 }
+function existsAsPassword(password){
+	for (i = 0; i < passwordList.length; i++){
+		if (password === passwordList[i]) return true;
+	}
+	return false;
+}
 
 
-//Back to server logic
+//Server code
 
 app.get('/', function(req, res) {
 	console.dir(req.param);
@@ -87,69 +73,28 @@ app.get('/', function(req, res) {
     res.writeHead(200, {'Content-Type': 'text/html'});
     res.end(html);
 })
-// app.post('/', function(req, res) {
-// 	console.log("in post /");
-// 	console.log("current turn = " + turn);
-// 	var body = '';
-// 	req.on('data', function (data) {
-// 		console.log("body data count = " + dataCount);
-// 		dataCount++;
-//         body += data;
-//     });
-//     req.on('end', function () {
-//     	console.log("Body: " + body);
-//     	dataCount = 0;
-//         handlePieceMsg(body);
-//         switchTurn();
-//     });
-//     res.writeHead(200, {'Content-Type': 'text/html'});
-//     reply = '';
-//     res.end(reply);
-// })
 
 app.post('/', function(req, res) {
-	var body = req.body;
-	/*
-	console.log("*** Placing piece: ***");
-	console.log("body = " + util.inspect(body, false, null));
-	console.log("starting checks");
-	console.log("body.length = " + body.length);
-	console.log("Object.keys(data) = " + Object.keys(body));
-	console.log("Object.keys(body)[0] = " + Object.keys(body)[0]);
-	console.log("JSON.parse(Object.keys(body)[0]) = " + JSON.parse(Object.keys(body)[0]));
-	console.log("util.inspect(JSON.parse(Object.keys(body)[0])) = " + util.inspect(JSON.parse(Object.keys(body)[0])));
-	console.log("JSON.parse(Object.keys(body)[0]).piece = " + JSON.parse(Object.keys(body)[0]).piece);
-	console.log("finished checks");
-	*/
-	var piece = JSON.parse(Object.keys(body)[0]).piece;
-	var playerCode = JSON.parse(Object.keys(body)[0]).playerCode;
-	console.log("piece = " + piece + ",   playerCode = " + playerCode);
-	handlePieceMsg(piece,playerCode);
-	switchTurn();
+	var bodyObject = JSON.parse(Object.keys(req.body)[0]);
+	var piece = bodyObject.piece;
+	var playerCode = bodyObject.playerCode;
+	var password = bodyObject.password;
+	password = "abc";
+	
+	if (!existsAsPassword(password)){
+		passwordList.push(password);
+		turn[password] = playerCode;
+		initBoard(password);	
+	}
+	addPieceToBoard(piece,playerCode,password);
+	switchTurn(password);
+	
 	res.writeHead(200, {'Content-Type': 'text/html'});
 	reply = boardToMsg();
     res.end(reply);
 })
 
-// app.post('/board', function(req, res) {
-// 	console.log("in post /board");
-// 	console.log("turn = " + turn);
-// 	var body = '';
-// 	req.on('data', function (data) {
-//         body += data;
-//     });
-//     req.on('end', function () {
-//     	console.log("not handling piece. Body: " + body);
-//     });
-//     res.writeHead(200, {'Content-Type': 'text/html'});
-//     reply = boardToMsg();
-//     console.log("reply = " + reply);
-//     res.end(reply);
-// })
-
 app.post('/board', function(req, res) {
-	//console.log("request = " + util.inspect(req, false, null));
-	//console.log("request = " + util.inspect(res, false, null));
 	res.writeHead(200, {'Content-Type': 'text/html'});
 	reply = boardToMsg();
     res.end(reply);
@@ -158,21 +103,3 @@ app.post('/board', function(req, res) {
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'))
 })
-
-
-/*
-//DB logic
-var MongoClient = require('mongodb').MongoClient
-  , assert = require('assert');
-
-//var url = 'mongodb://localhost:27017/gamedata';
-var url = 'mongodb://jack:password@ds139187.mlab.com:39187/heroku_5l1pksgk';
-
-
-MongoClient.connect(url, function(err, db) {
-  assert.equal(null, err);
-  console.log("Connected successfully to server");
-
-  db.close();
-});
-*/
