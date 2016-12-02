70d18ecc6c4b63c8bd4da53bc0eb7495db355b59
Jack Walker <jmw2g12@soton.ac.uk>
2016-12-02 16:44:43 +0000

    automatic commit

--- server.js
+++ server.js
@@ -297,8 +297,8 @@ app.post(/.*\/board/ ,function(req, res) {
     res.end(JSON.stringify({a:1,b:2}));
 })
 
-app.post(/.*\/b.*/ ,function(req, res) {
-	console.log('this accepts anything b');
+app.post(/.*\/.*board.*/ ,function(req, res) {
+	console.log('this accepts anything board');
 	res.writeHead(200, {'Content-Type': 'text/html'});
     res.end(JSON.stringify({a:1,b:2}));
 })
