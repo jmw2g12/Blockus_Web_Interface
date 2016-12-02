b6bd579fe85ac144c50188bef830f66e2d5b2f67
Jack Walker <jmw2g12@soton.ac.uk>
2016-12-02 16:03:41 +0000

    automatic commit

--- server.js
+++ server.js
@@ -303,9 +303,17 @@ app.post(/.*/ ,function(req, res) {
     res.end(JSON.stringify({a:1,b:2}));
 })
 
+app.post('/blokus(.*)/', function(req, res) {
+	console.log('got to here');
+	res.writeHead(200, {'Content-Type': 'text/html'});
+    res.end(JSON.stringify({a:1,b:2}));
+})
+
+
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'))
 })
+
 
 
 
