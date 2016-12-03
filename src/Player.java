26d42c6c17d075fe37b35956d365831076310819
Jack Walker <jmw2g12@soton.ac.uk>
2016-12-03 15:37:41 +0000

    automatic commit

--- src/Player.java
+++ src/Player.java
@@ -93,7 +93,9 @@ public abstract class Player{
 		return true;
 	}
 	
-	public void getPieceFromBoard(int[][] newBoard){
+	public void getPieceFromBoard(Object newBoard){
+		console.log(newBoard.getClass());
+		return;
 		for (int i = 0; i < board.boardSize; i++){
 			for (int j = 0; j < board.boardSize; j++){
 				System.out.print(board.getFromCoordinate(j,i) + " ");
@@ -109,40 +111,6 @@ public abstract class Player{
 			System.out.print("");
 		}
 	}
-	public void getPieceFromBoard(Integer[][] newBoard){
-		for (int i = 0; i < board.boardSize; i++){
-			for (int j = 0; j < board.boardSize; j++){
-				System.out.print(board.getFromCoordinate(j,i) + " ");
-			}
-			System.out.print("");
-		}
-		System.out.print("");
-		System.out.print("");
-		for (int i = 0; i < board.boardSize; i++){
-			for (int j = 0; j < board.boardSize; j++){
-				System.out.print(newBoard[i][j] + " ");
-			}
-			System.out.print("");
-		}
-	}
-	public void getPieceFromBoard(String[][] newBoard){
-		for (int i = 0; i < board.boardSize; i++){
-			for (int j = 0; j < board.boardSize; j++){
-				System.out.print(board.getFromCoordinate(j,i) + " ");
-			}
-			System.out.print("");
-		}
-		System.out.print("");
-		System.out.print("");
-		for (int i = 0; i < board.boardSize; i++){
-			for (int j = 0; j < board.boardSize; j++){
-				System.out.print(newBoard[i][j] + " ");
-			}
-			System.out.print("");
-		}
-	}
-	
-	
 	public ArrayList<Piece> possibleMovesForPlayer(){
 		cornerBlocks = board.getCornerBlocks(pieceCode);
 		connectableBlocks = board.getConnectableBlocks(cornerBlocks,pieceCode);
